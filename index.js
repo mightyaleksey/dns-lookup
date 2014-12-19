7ccc04e9f5f2d5fb3b1b55c46206a669e47bfb1d
Alexey Litvinov <sullenor@yandex-team.ru>
2014-12-19 12:24:25 +0300

    also resolves domain into ipv4 address, if ipv6 not found

--- index.js
+++ index.js
@@ -5,20 +5,36 @@ var inherits = require('util').inherits;
 
 var EventEmitter = require('events').EventEmitter;
 
+/**
+ * @param  {String}   host
+ * @param  {Object}   options
+ * @param  {Function} callback
+ * @return {Resolver}
+ */
 function Resolver(host, options, callback) {
   if (typeof options === 'function') {
     callback = options;
     options = {};
+  } else {
+    options = options || {};
   }
 
   EventEmitter.call(this);
 
-  this.once('address', function (addr) {
-    callback(null, addr);
+  var that = this;
+
+  this.once('address', function (addr, family) {
+    that.removeAllListeners();
+    callback(null, addr, family);
   });
 
-  this.once('error', function (err) {
-    callback(err, null);
+  this.on('error', function (err, family) {
+    if (err.code === 'ENOTFOUND' && family === 6) {
+      that._lookup(host, 4);
+    } else {
+      that.removeAllListeners();
+      callback(err, null, family);
+    }
   });
 
   this._lookup(host, 6);
@@ -26,14 +42,18 @@ function Resolver(host, options, callback) {
 
 inherits(Resolver, EventEmitter);
 
+/**
+ * @param  {String} host
+ * @param  {Number} family 4|6.
+ */
 Resolver.prototype._lookup = function (host, family) {
   var that = this;
 
   dns.lookup(host, family, function (err, addr) {
     if (err) {
-      that.emit('error', err);
+      that.emit('error', err, family);
     } else {
-      that.emit('address', addr);
+      that.emit('address', addr, family);
     }
   });
 };
