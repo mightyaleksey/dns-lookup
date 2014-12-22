633d6b9bf1e6cc777104e48f715739393e0ae15c
Alexey Litvinov <sullenor@yandex-team.ru>
2014-12-22 17:31:54 +0300

    new library name

--- test/index.js
+++ test/index.js
@@ -2,11 +2,11 @@ describe('index.js', function () {
     'use strict';
 
     var expect = require('expect.js');
-    var resolve = require('../index.js');
+    var lookup = require('..');
     var uri = 'www.yandex.ru';
 
     it('returns ip address for the specified domain', function (done) {
-        resolve(uri, function (err, address) {
+        lookup(uri, function (err, address) {
             expect(err).to.not.be.ok();
             expect(address).to.be.ok();
             expect(address).to.be.an('string');
