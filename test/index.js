ef9b5259ee1d4078b06925c7262008d8a3680f46
Alexey Litvinov <sullenor@yandex-team.ru>
2015-01-12 17:37:59 +0300

    new tests

--- test/index.js
+++ test/index.js
@@ -1,16 +1,54 @@
 describe('index.js', function () {
-    'use strict';
+  'use strict';
 
-    var expect = require('expect.js');
-    var lookup = require('..');
-    var uri = 'www.yandex.ru';
+  var expect = require('expect.js');
+  var lookup = require('..');
 
-    it('returns ip address for the specified domain', function (done) {
-        lookup(uri, function (err, address) {
-            expect(err).to.not.be.ok();
-            expect(address).to.be.ok();
-            expect(address).to.be.an('string');
-            done(err);
-        });
+  describe('for the "yandex.ru" domain', function () {
+    var ipAddress;
+    var addressFamily;
+
+    before(function (next) {
+      var uri = 'www.yandex.ru';
+
+      lookup(uri, function (err, address, family) {
+        ipAddress = address;
+        addressFamily = family;
+        next();
+      });
     });
+
+    it('returns ip address', function () {
+      expect(ipAddress).to.be.a('string');
+    });
+
+    it('returns ip address family', function () {
+      expect(addressFamily).to.be.an('number');
+    });
+  });
+
+  describe('for the unexisting domain', function () {
+    var error;
+    var ipAddress;
+
+    before(function (next) {
+      var uri = 'qwe.rty555';
+
+      lookup(uri, function (err, address) {
+        error = err;
+        ipAddress = address;
+        next();
+      });
+    });
+
+    it('returns error with code ENOTFOUND', function () {
+      expect(error).to.be.an('object');
+      expect(error).to.have.property('code');
+      expect(error.code).to.be.equal('ENOTFOUND');
+    });
+
+    it('returns null instead of ip address', function () {
+      expect(ipAddress).to.be.equal(null);
+    });
+  });
 });
